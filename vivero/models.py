8f25e3dc0fd16946f99c0f1cf7eb812f41a682d4
jondasa <jonathan.salgado@utp.edu.co>
2024-11-19 15:27:44 -0500

    #HU-1

--- vivero/models.py
+++ vivero/models.py
@@ -59,3 +59,24 @@ class Labor(models.Model):
 
     def __str__(self):
         return f"Labor {self.descripcion} - {self.fecha}"
+    
+#HU-1: Registro de Productos en el Inventario
+
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    cantidad = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.nombre    
+    
