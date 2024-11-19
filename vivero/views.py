2f9b49980a963361dc8973037e515e352734b84c
jondasa <jonathan.salgado@utp.edu.co>
2024-11-19 15:52:07 -0500

    #HU-1.

--- vivero/views.py
+++ vivero/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django import forms
+from .models import Producto
+from django.shortcuts import render, redirect
 
 # Create your views here.
+class ProductoForm(forms.ModelForm):
+    class Meta:
+        model = Producto
+        fields = ['nombre', 'descripcion', 'categoria', 'precio', 'cantidad']
+
+
+
+
+
+def registrar_producto(request):
+    if request.method == "POST":
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_productos')
+    else:
+        form = ProductoForm()
+    return render(request, 'productos/registrar_producto.html', {'form': form})
+
+def listar_productos(request):
+    productos = Producto.objects.all()
+    return render(request, 'productos/listar_productos.html', {'productos': productos})
